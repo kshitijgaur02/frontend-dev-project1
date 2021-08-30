b9fb36cb161d3b0b51cdef8e840cf9ceb4db41a1
kshitijgaur02 <iamkshitijgaur@gmail.com>
2021-08-30 21:07:47 +0530

    minor changes

--- wallet.js
+++ wallet.js
@@ -1,25 +1,19 @@
-const mediaQuery = window.matchMedia('(max-width: 640px)')
+const mediaQuery = window.matchMedia("(max-width: 640px)");
 
 let nftCLick = document.getElementById("nft-details");
 let balanceClick = document.getElementById("balance-details");
 
-
 if (mediaQuery.matches) {
+  balanceClick.style.display = "block";
+  nftCLick.style.display = "none";
+
+  function nft() {
+    nftCLick.style.display = "block";
+    balanceClick.style.display = "none";
+  }
+
+  function balance() {
     balanceClick.style.display = "block";
-    nftCLick.style.display = "none"
-    
-    function nft() {
-        nftCLick.style.display = "block";
-        balanceClick.style.display = "none";
-    } 
-    
-    function balance() {
-        balanceClick.style.display  = "block";
-        nftCLick.style.display = "none";
-    }
-}   
-
-
-
-
-
+    nftCLick.style.display = "none";
+  }
+}
